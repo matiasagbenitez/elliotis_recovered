17f053d1d72394a66fb1b3b4ccd8a72da41e669b
Matías <matiasagbenitez@gmail.com>
2022-11-10 08:21:12 -0300

    En caso de que una oferta sea cancelada, esto se muestra en el detalle

--- resources/views/livewire/tenderings/show-offer-tendering.blade.php
+++ resources/views/livewire/tenderings/show-offer-tendering.blade.php
@@ -25,48 +25,69 @@
     <div class="px-6 py-3 bg-white rounded-lg shadow mt-4">
         <p class="font-bold my-1">Oferta de {{ $supplier->business_name }}</p>
         <hr class="mt-1 mb-2">
-        <div>
-            <p class="text-sm font-bold my-1">Visto el:
-                <span class="font-normal">
-                    @if ($hash->seen)
-                        {{ Date::parse($offer->hash->seen_at)->format('d-m-Y H:i') }} hs
-                    @else
-                        No visto
-                    @endif
-                </span>
-            </p>
-            <p class="text-sm font-bold my-1">Fecha de respuesta inicial:
-                <span class="font-normal">
-                    @if ($hash->answered)
-                        {{ Date::parse($offer->hash->answered_at)->format('d-m-Y H:i') }} hs
-                    @else
-                        No respondido
-                    @endif
-                </span>
-            </p>
-            <p class="text-sm font-bold my-1">Fecha última modificación:
-                <span class="font-normal">
-                    @if ($hash->answered)
-                        {{ Date::parse($offer->updated_at)->format('d-m-Y H:i') }} hs
-                    @else
-                        No modificado
-                    @endif
-                </span>
-            </p>
-            <p class="text-sm font-bold my-1">Fecha de cancelación:
-                <span class="font-normal">
-                    @if ($offer->hash->cancelled)
-                        {{ Date::parse($offer->hash->canceled_at)->format('d-m-Y H:i') }}
-                    @else
-                        No cancelado
-                    @endif
-                </span>
-            </p>
-            <p class="text-sm font-bold my-1">Hash:
-                <span class="font-normal">{{ $offer->hash->hash }}</span>
-            </p>
+        <div class="grid grid-cols-2">
+            <div>
+                <p class="text-sm font-bold my-1">Concurso visto por primera vez el:
+                    <span class="font-normal">
+                        @if ($hash->seen)
+                            {{ Date::parse($offer->hash->seen_at)->format('d-m-Y H:i') }} hs
+                        @else
+                            No visto
+                        @endif
+                    </span>
+                </p>
+                <p class="text-sm font-bold my-1">Fecha de respuesta inicial:
+                    <span class="font-normal">
+                        @if ($hash->answered)
+                            {{ Date::parse($offer->hash->answered_at)->format('d-m-Y H:i') }} hs
+                        @else
+                            No respondido
+                        @endif
+                    </span>
+                </p>
+                <p class="text-sm font-bold my-1">Fecha última modificación:
+                    <span class="font-normal">
+                        @if ($hash->answered)
+                            {{ Date::parse($offer->updated_at)->format('d-m-Y H:i') }} hs
+                        @else
+                            No modificado
+                        @endif
+                    </span>
+                </p>
+                {{-- <p class="text-sm font-bold my-1">Fecha de cancelación:
+                    <span class="font-normal">
+                        @if ($offer->hash->cancelled)
+                            {{ Date::parse($offer->hash->canceled_at)->format('d-m-Y H:i') }}
+                        @else
+                            No cancelado
+                        @endif
+                    </span>
+                </p> --}}
+                <p class="text-sm font-bold my-1">Hash:
+                    <span class="font-normal">{{ $offer->hash->hash }}</span>
+                </p>
+            </div>
+
+            @if ($offer->hash->cancelled)
+                <div class="flex justify-center items-center">
+                    <div class="border-2 border-red-600 rounded px-5 py-1 flex items-center gap-3 w-3/4">
+                        <i class="fas fa-ban text-red-600 text-3xl"></i>
+                        <div>
+                            <p class="text-red-600 uppercase font-bold text-sm">
+                                Oferta cancelada por el proveedor el día
+                                {{ Date::parse($offer->hash->canceled_at)->format('d-m-Y') }}
+                                a las
+                                {{ Date::parse($offer->hash->canceled_at)->format('H:i') }}
+                                .
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
-        <hr class="my-2">
+
+        <p class="font-bold mt-2">Detalle última oferta</p>
+        <hr class="mt-1 mb-2">
 
         {{-- DETALLE DE OFERTA --}}
         <x-responsive-table>
@@ -117,17 +138,17 @@
         </x-responsive-table>
         <div class="mt-3">
             <div>
-                <p class="text-sm font-bold my-1">Subtotal:
+                <p class="text-sm font-bold my-1">Subtotal =
                     <span class="font-normal">${{ number_format($offer->subtotal, 2, ',', '.') }}</span>
                 </p>
             </div>
             <div>
-                <p class="text-sm font-bold my-1">IVA:
+                <p class="text-sm font-bold my-1">IVA =
                     <span class="font-normal">${{ number_format($offer->iva, 2, ',', '.') }}</span>
                 </p>
             </div>
             <div>
-                <p class="text-sm font-bold my-1">Total final:
+                <p class="text-sm font-bold my-1">Total final =
                     <span class="font-normal">${{ number_format($offer->total, 2, ',', '.') }}</span>
                 </p>
             </div>
