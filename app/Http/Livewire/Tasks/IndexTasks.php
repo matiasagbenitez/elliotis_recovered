1b3ea3384d8ed630a8e9c5e4e354a794178208a2
Matías <matiasagbenitez@gmail.com>
2022-12-02 21:57:04 -0300

    Trabajos sobre el index de tareas para mostrar el estado de trabajo en cada tipo de tarea

--- app/Http/Livewire/Tasks/IndexTasks.php
+++ app/Http/Livewire/Tasks/IndexTasks.php
@@ -2,16 +2,60 @@
 
 namespace App\Http\Livewire\Tasks;
 
-use App\Models\TaskType;
+use App\Models\User;
 use Livewire\Component;
+use App\Models\TaskType;
+use Illuminate\Support\Facades\Date;
 
 class IndexTasks extends Component
 {
-    public $tasksTypes;
+    public $tasksTypes, $stats;
 
     public function mount()
     {
         $this->tasksTypes = TaskType::all();
+        $this->stats = $this->getStats();
+    }
+
+    public function getStats()
+    {
+        $stats = [];
+
+        foreach ($this->tasksTypes as $taskType) {
+
+            $running_task = $taskType->tasks()->where('task_status_id', 2)->first();
+            $running_task ? $task = $running_task : $task = $taskType->tasks()->latest()->first();
+
+            $stats[] = [
+                'id' => $taskType->id,
+                'task_id' => $running_task ? $task->id : null,
+                'name' => $taskType->name,
+                'icon' => $taskType->icon,
+                'running_task' => $task ? ($task->task_status_id == 2 ? true : false) : null,
+                'user' =>
+                    $task ?
+                    (
+                        $task->task_status_id == 2
+                        ?
+                        User::find($task->started_by)->name
+                        :
+                        User::find($task->finished_by)->name
+                    ) : null,
+                'date' =>
+                $task ?
+                (
+                    $task->task_status_id == 2
+                    ?
+                    Date::parse($task->started_at)->format('d/m/Y H:i')
+                    :
+                    Date::parse($task->finished_at)->format('d/m/Y H:i')
+                ) : null,
+            ];
+        }
+
+        // dd($stats);
+
+        return $stats;
     }
 
     public function render()
--- resources/views/livewire/tasks/index-tasks.blade.php
+++ resources/views/livewire/tasks/index-tasks.blade.php
@@ -11,13 +11,40 @@
         <hr class="my-2">
         <div class="grid grid-cols-3 gap-4">
 
-            @foreach ($tasksTypes as $taskType)
-                <a href="{{ route('admin.tasks.manage', $taskType) }}">
+            @foreach ($stats as $stat)
+                <a href="{{ route('admin.tasks.manage', $stat['id']) }}">
                     <div class="hover:bg-gray-50 hover:shadow-lg rounded-lg p-2 hover:cursor-pointer">
                         <div class="flex justify-center">
-                            <img class="h-40 w-40" src="{{ asset($taskType->icon) }}" alt="Ícono de la tarea">
+                            <img class="h-40 w-40" src="{{ asset($stat['icon']) }}" alt="Ícono de la tarea">
                         </div>
-                        <h2 class="uppercase font-bold text-center my-3">{{ $taskType->name }}</h2>
+
+                        <h2 class="uppercase font-bold text-center my-3">{{ $stat['name'] }}</h2>
+
+                        @if ($stat['running_task'] == true)
+                            <div class="space-y-1 text-center p-3 font-semibold">
+                                <span
+                                    class="px-6 py-1 mb-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                    LÍNEA EN FUNCIONAMIENTO
+                                </span>
+                                @if (isset($stat['running_task']))
+                                    <p class="text-sm">Última tarea iniciada por</p>
+                                    <p class="text-sm">{{ $stat['user'] }}</p>
+                                    <p class="text-sm">el {{ $stat['date'] }}hs</p>
+                                @endif
+                            </div>
+                        @elseif ($stat['running_task'] == false)
+                            <div class="space-y-1 text-center p-3 font-semibold ">
+                                <span
+                                    class="px-6 py-1 mb-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                    LÍNEA DETENIDA
+                                </span>
+                                @if (isset($stat['running_task']))
+                                    <p class="text-sm">Última tarea finalizada por</p>
+                                    <p class="text-sm">{{ $stat['user'] }}</p>
+                                    <p class="text-sm">el {{ $stat['date'] }}hs</p>
+                                @endif
+                            </div>
+                        @endif
                     </div>
                 </a>
             @endforeach
