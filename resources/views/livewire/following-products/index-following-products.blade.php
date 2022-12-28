00bd83a22041bc1edaf1117301413e8d4e432f02
Matías <matiasagbenitez@gmail.com>
2022-12-28 17:50:58 -0300

    Productos de salida: modelo, migración, seeder, relaciones y vista index OK

---
+++ resources/views/livewire/following-products/index-following-products.blade.php
@@ -0,0 +1,60 @@
+<div class="container py-6">
+
+    <x-slot name="header">
+        <div class="flex items-center justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Productos de salida</h2>
+        </div>
+    </x-slot>
+
+    <x-responsive-table>
+
+        <div class="px-6 py-4">
+            <h1 class="text-lg pb-2 font-semibold">Seleccione un producto de entrada para ver sus posibles productos de salida</h1>
+            <select wire:model='filter' class="input-control w-full">
+                <option disabled value="">Seleccione una opción</option>
+                @foreach ($allProducts as $product)
+                    <option value="{{ $product->id }}">{{ $product->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        @if ($following_products != null)
+            <table class="text-gray-600 min-w-full divide-y divide-gray-200">
+                <thead class="border-b border-gray-300 bg-gray-200">
+                    <tr class="text-center text-sm text-gray-500 uppercase">
+                        <th scope="col"
+                            class="px-4 py-2">
+                            ID
+                        </th>
+                        <th scope="col"
+                            class="px-4 py-2 w-full">
+                            Nombre
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200">
+                    @foreach ($following_products as $phase)
+                        <tr class="bg-gray-50">
+                            <td class="px-6 py-3">
+                                <p class="text-sm uppercase">
+                                    {{ $phase->id }}
+                                </p>
+                            </td>
+                            <td class="px-6 py-3 text-center">
+                                <p class="text-sm uppercase">
+                                    {{ $phase->name }}
+                                </p>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <div class="px-6 py-4">
+                <p class="text-center font-semibold">No se encontraron registros coincidentes.</p>
+            </div>
+        @endif
+
+    </x-responsive-table>
+
+</div>
