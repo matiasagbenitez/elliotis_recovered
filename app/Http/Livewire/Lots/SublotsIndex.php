8f0ac713b50c37f74416d2a8a16fdfeb9209bbb3
Matías <matiasagbenitez@gmail.com>
2023-01-03 11:41:26 -0300

    Mejoras en las vistas de sublotes

--- app/Http/Livewire/Lots/SublotsIndex.php
+++ app/Http/Livewire/Lots/SublotsIndex.php
@@ -3,15 +3,24 @@
 namespace App\Http\Livewire\Lots;
 
 use App\Models\Lot;
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Date;
 
 class SublotsIndex extends Component
 {
     public $lot;
+    public $lotData;
 
     public function mount(Lot $lot)
     {
         $this->lot = $lot;
+        $this->lotData = [
+            'taskName' => $lot->task->typeOfTask->name,
+            'finishedBy' => User::find($lot->task->finished_by)->name,
+            'finishedAt' => Date::parse($lot->task->finished_at)->format('d-m-Y H:i'),
+            'taskId' => $lot->task->id
+        ];
     }
 
     public function render()
--- resources/views/livewire/lots/sublots-index.blade.php
+++ resources/views/livewire/lots/sublots-index.blade.php
@@ -6,69 +6,69 @@
         </div>
     </x-slot>
 
-    <div class="bg-white px-8 pt-4 pb-8 my-4 rounded-lg">
-        <div class="my-3">
-            <span class="font-bold text-gray-500">
-                <i class="fas fa-info-circle mr-1"></i>
-                Lote {{ $lot->code }}
-            </span>
-            <hr class="my-2">
-        </div>
+    <x-responsive-table>
 
-        <div class="grid grid-cols-6 gap-4">
-            <div class="col-span-2">
-                <x-jet-label class="mb-2 font-bold" value="Tipo de tarea que generó el lote" />
-                <x-jet-input type="text" class="w-full text-gray-500" disabled
-                    value="{{ $lot->task->typeOfTask->name }}" />
+        <div class="bg-gray-100 px-4 mt-4 mb-6 rounded-lg">
+            <div class="my-3">
+                <span class="font-bold text-gray-500">
+                    <i class="fas fa-info-circle mr-1"></i>
+                    Lote {{ $lot->code }}
+                </span>
             </div>
 
-            <div class="col-span-2">
-                <x-jet-label class="mb-2 font-bold" value="Lote generado por" />
-                <x-jet-input type="text" class="w-full text-gray-500" disabled value="#" />
-            </div>
+            <div class="md:grid md:grid-cols-6 gap-4">
+                <div class="md:col-span-3">
+                    <x-jet-label class="mb-2 font-bold text-gray-600" value="Tipo de tarea que generó el lote" />
+                    <x-jet-input type="text" class="w-full text-gray-500" disabled
+                        value="{{ $lotData['taskName'] }}" />
+                </div>
 
-            <div class="col-span-2">
-                <x-jet-label class="mb-2 font-bold" value="Lote generado el" />
-                <x-jet-input type="text" class="w-full text-gray-500" disabled value="{{ $lot->created_at }}" />
+                <div class="md:col-span-1">
+                    <x-jet-label class="mb-2 font-bold text-gray-600" value="Usuario generador" />
+                    <x-jet-input type="text" class="w-full text-gray-500" disabled value="{{ $lotData['finishedBy'] }}" />
+                </div>
+
+                <div class="md:col-span-1">
+                    <x-jet-label class="mb-2 font-bold text-gray-600" value="Fecha generación" />
+                    <x-jet-input type="text" class="w-full text-gray-500" disabled value="{{ $lotData['finishedAt']  }}" />
+                </div>
+
+                <div class="md:col-span-1">
+                    <x-jet-label class="mb-2 font-bold text-gray-600" value="Tarea generadora" />
+                    <x-jet-input type="text" class="w-full text-gray-500" disabled value="{{ $lotData['taskId']  }}" />
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="bg-white px-8 pt-4 pb-8 my-4 rounded-lg">
-        <div class="my-3">
-            <span class="font-bold text-gray-500">
-                <i class="fas fa-info-circle mr-1"></i>
-                Detalle de sublotes
-            </span>
-            <hr class="my-2">
-        </div>
-
-
-        <table class="text-gray-600 min-w-full divide-y divide-gray-200 table-fixed">
-            <thead class="text-sm text-center text-gray-700 uppercase border-b bg-gray-50">
-                <tr>
-                    <th scope="col" class="px-4 py-2 whitespace-nowrap">
-                        ID sublote
-                    </th>
-                    <th scope="col" class="px-4 py-2">
-                        Código
-                    </th>
-                    <th scope="col" class="px-4 py-2">
-                        Producto
-                    </th>
-                    <th scope="col" class="px-4 py-2">
-                        Cantidad inicial
-                    </th>
-                    <th scope="col" class="px-4 py-2">
-                        Cantidad actual
-                    </th>
-                    <th scope="col" class="px-4 py-2">
-                        Disponibilidad
-                    </th>
-                </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-200">
-                @foreach ($lot->sublots as $sublot)
+        @if ($lot != null)
+            <table class="text-gray-600 min-w-full divide-y divide-gray-200">
+                <thead class="border-b border-gray-300 bg-gray-200">
+                    <tr class="text-center text-sm text-gray-500 uppercase">
+                        <th scope="col" class="px-4 py-2 whitespace-nowrap">
+                            ID sublote
+                        </th>
+                        <th scope="col" class="px-4 py-2">
+                            Código
+                        </th>
+                        <th scope="col" class="px-4 py-2">
+                            Producto
+                        </th>
+                        <th scope="col" class="px-4 py-2">
+                            Ubicación
+                        </th>
+                        <th scope="col" class="px-4 py-2">
+                            Cantidad inicial
+                        </th>
+                        <th scope="col" class="px-4 py-2">
+                            Cantidad actual
+                        </th>
+                        <th scope="col" class="px-4 py-2">
+                            Disponibilidad
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200">
+                    @foreach ($lot->sublots as $sublot)
                     <tr>
                         <td class="px-6 py-2 text-center">
                             <p class="text-sm uppercase">
@@ -83,6 +83,11 @@
                         <td class="px-6 py-2 text-center">
                             <p class="text-sm uppercase">
                                 {{ $sublot->product->name }}
+                            </p>
+                        </td>
+                        <td class="px-6 py-2 text-center">
+                            <p class="text-sm uppercase">
+                                {{ $sublot->area->name }}
                             </p>
                         </td>
                         <td class="px-6 py-2 text-center">
@@ -112,9 +117,14 @@
 
                     </tr>
                 @endforeach
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        @else
+            <div class="px-6 py-4">
+                <p class="text-center font-semibold">No se encontraron registros coincidentes.</p>
+            </div>
+        @endif
 
-    </div>
+    </x-responsive-table>
 
 </div>
